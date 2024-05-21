e9a5ffad694be2558be55a1c711148d54654f029
Sunnidhya Roy <sunnidhya.roy@gmail.com>
2024-05-21 12:21:41 +0530

    Updated test

--- Backend/test_backend.py
+++ Backend/test_backend.py
@@ -6,27 +6,29 @@ from app import app
 class TestBackend(unittest.TestCase):
 
     def test_list_of_movies(self):
-        response = requests.get('/listofmovies')
-        
-        self.assertEqual(response.status_code, 200)
-        data = response.json()
-        print("Response from /listofmovies endpoint:", data)
-        self.assertIsInstance(data, list)
-        self.assertTrue(len(data) > 0)
+        # response = requests.get('http://localhost:5000/listofmovies')
+
+        with app.test_client() as client:
+            response = client.get('/listofmovies')
+            self.assertEqual(response.status_code, 200)
+            data = response.json()
+            print("Response from /listofmovies endpoint:", data)
+            self.assertIsInstance(data, list)
+            self.assertTrue(len(data) > 0)
         
 
     def test_process(self):
-        
         payload = json.dumps(
             [{
             "movie": "Titan A.E. (2000)",
             "rating":"5"
             }]
         )
-        headers = {'Content-Type': 'application/json'}
-        response = requests.post('/moviename', data=payload, headers=headers)
-        self.assertEqual(response.status_code, 200)
-        print("Response from /moviename endpoint:", response.text)
+        with app.test_client() as c:
+            headers = {'Content-Type': 'application/json'}
+            response = c.post('/moviename', data=payload, headers=headers)
+            self.assertEqual(response.status_code, 200)
+            print("Response from /moviename endpoint:", response.text)
         
 
 if __name__ == '__main__':
